83ba016c3924d8a119347fd32ca94f0e4a275667
bahrmichael <mbahr92@gmail.com>
2017-07-15 15:10:10 +0200

    added details to Item

--- src/main/java/de/jtheb/fh/lagerverwaltung/entities/Item.java
+++ src/main/java/de/jtheb/fh/lagerverwaltung/entities/Item.java
@@ -1,4 +1,9 @@
 package de.jtheb.fh.lagerverwaltung.entities;
 
 public class Item {
+    private String id;
+    private String name;
+    private int height;
+    private int width;
+    private int depth;
 }
