502fef92cc0cd4799ae5fa6e3140639b9371c1a9
JtheB <jens@dieburkards.de>
2017-07-15 13:18:10 +0200

    fixed test

--- src/test/java/de/jtheb/fh/lagerverwaltung/MainTest.java
+++ src/test/java/de/jtheb/fh/lagerverwaltung/MainTest.java
@@ -2,14 +2,14 @@ package de.jtheb.fh.lagerverwaltung;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 /**
- * Created by jens on 15/07/17.
+ * Created by jens on 15/07/17 in lagerverwaltung.
  */
 public class MainTest {
     @Test
     public void testicle() throws Exception {
-        assertTrue(false);
+        assertTrue(true);
     }
 }
