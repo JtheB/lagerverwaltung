7b9d7d6aba0cd55fce941f2a7b0751f72379a6cc
bahrmichael <mbahr92@gmail.com>
2017-07-15 15:11:07 +0200

    added details to Compartment and generated getters/setters

--- src/main/java/de/jtheb/fh/lagerverwaltung/entities/Compartment.java
+++ src/main/java/de/jtheb/fh/lagerverwaltung/entities/Compartment.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 public class Compartment {
 
+    private int height = 200;
+    private int width = 200;
+    private int depth = 200;
+
     List<Item> items = new ArrayList<>();
 
     public Compartment() {
@@ -25,5 +29,29 @@ public class Compartment {
 
     public void setItems(final List<Item> items) {
         this.items = items;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(final int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(final int width) {
+        this.width = width;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public void setDepth(final int depth) {
+        this.depth = depth;
     }
 }
--- src/main/java/de/jtheb/fh/lagerverwaltung/entities/Item.java
+++ src/main/java/de/jtheb/fh/lagerverwaltung/entities/Item.java
@@ -6,4 +6,44 @@ public class Item {
     private int height;
     private int width;
     private int depth;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(final String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(final int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(final int width) {
+        this.width = width;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public void setDepth(final int depth) {
+        this.depth = depth;
+    }
 }
