f5d5682d898a1905424fab603cc5974e4029a244
JtheB <jens@dieburkards.de>
2017-07-15 13:06:10 +0200

    Initial commit

---
+++ src/main/java/de/jtheb/fh/lagerverwaltung/Main.java
@@ -0,0 +1,7 @@
+package de.jtheb.fh.lagerverwaltung;
+
+/**
+ * Created by jens on 15/07/17.
+ */
+public class Main {
+}
---
+++ src/test/java/de/jtheb/fh/lagerverwaltung/MainTest.java
@@ -0,0 +1,15 @@
+package de.jtheb.fh.lagerverwaltung;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jens on 15/07/17.
+ */
+public class MainTest {
+    @Test
+    public void testicle() throws Exception {
+        assertTrue(false);
+    }
+}
